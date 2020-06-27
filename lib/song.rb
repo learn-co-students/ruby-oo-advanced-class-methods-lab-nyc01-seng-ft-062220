e5b9448cbdc423458b6f45f71396ac25203f51a9
hahm-d <dhahm01@gmail.com>
2020-06-26 23:12:57 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
 class Song
   attr_accessor :name, :artist_name
+  attr_reader
   @@all = []
+
+  def self.create
+    song = self.new 
+    song.save
+    return song 
+  end
 
   def self.all
     @@all
@@ -10,4 +17,58 @@ class Song
     self.class.all << self
   end
 
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    return song  
+  end
+  def self.create_by_name(name) 
+    song = self.new 
+    song.name = name 
+    song.save
+    return song 
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+      if song.name == name
+        return song 
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(name) #return a matching song instance with that name or create a new song with the name and return the song instance
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+      self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+    file = filename.split(" - ")
+    artist = file [0]
+    mp3 = file[1].split(".")
+    name =  mp3[0]
+    song = self.new
+    song.name = name
+    song.artist_name = artist
+    return song
+  end
+
+  def self.create_from_filename(filename)
+    file = filename.split(" - ")
+    artist = file [0]
+    mp3 = file[1].split(".")
+    name =  mp3[0]
+    song = self.create
+    song.name = name
+    song.artist_name = artist
+    return song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
+
